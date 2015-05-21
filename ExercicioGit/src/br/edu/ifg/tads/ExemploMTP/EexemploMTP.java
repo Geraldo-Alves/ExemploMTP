db180d529af9a96ca53dbc63de4176e72225a137
Geraldo-Alves <geraldoalves11@hotmail.com>
2015-05-21 11:23:24 -0400

    Primeiro Commit

---
+++ ExercicioGit/src/br/edu/ifg/tads/ExemploMTP/EexemploMTP.java
@@ -0,0 +1,8 @@
+//Author: Geraldo Alves Pereira Júnior
+package br.edu.ifg.tads.ExemploMTP;
+
+public class EexemploMTP {
+	public static void main(String[] args) {
+		System.out.println("Exemplo MTP, Exercício 1 do GIT");
+	}
+}
